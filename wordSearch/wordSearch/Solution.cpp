e0f8f0f6f349ba4f8ed4ec6ba7e5fd3bf79bf94c
sunRainPenguin <1049590822@qq.com>
2017-08-28 17:18:27 +0800

    update

--- wordSearch/wordSearch/Solution.cpp
+++ wordSearch/wordSearch/Solution.cpp
@@ -2,8 +2,15 @@
 
 int main()
 {
-	vector<vector<char>> board = { {'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'} };
+	vector<vector<char>> board = { {'a','a','a','a'},{ 'a','a','a','a' },{ 'a','a','a','a' },{ 'a','a','a','a' },{ 'b','c','d','e' },{ 'f','g','h','i' },{ 'j','k','l','m' },{ 'n','o','p','q' },{ 'r','s','t','u' },{ 'v','w','x','y' },{ 'z','z','z','z' }};// { { 'b','a','a','b','a','b' }, { 'a','b','a','a','a','a' }, { 'a','b','a','a','a','b' }, { 'a','b','a','b','b','a' },{ 'a','a','b','b','a','b' },{ 'a','a','b','b','b','a' },{ 'a','a','b','a','a','b' } };	// { {'o'},{'a'},{'t'},{'x'} }; { 'o','a','a','n' },{ 'e','t','a','e' },{ 'i','h','k','r' },{ 'i','f','l','v' }
 	Solution solution;
-	cout << solution.findWord(board, "oath") << endl;
+	vector<string> inputs = { "aaaaaaaaaaaaaaai","aaaaaaaaaaaaaaaj","aaaaaaaaaaaaaaak","aaaaaaaaaaaaaaal","aaaaaaaaaaaaaaam","aaaaaaaaaaaaaaan","aaaaaaaaaaaaaaao","aaaaaaaaaaaaaaap" };
+	vector<string> vecStr = solution.findWords(board, inputs);
+	for (int i = 0; i < vecStr.size(); i++)
+	{
+		cout << vecStr[i] << endl;
+	}
 	return 0;
 }
+//
+//["aabbb babba ababa aaaba babba aba", "abaabbbaaaaababbbaaaaabbbaab", "ababaababaaabbabbaabbaabbaba"]
--- wordSearch/wordSearch/Solution.h
+++ wordSearch/wordSearch/Solution.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <stack>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 struct Pos {
@@ -10,15 +11,60 @@ struct Pos {
 	int y;
 };
 
+//按照结构体中字符串str的字典顺序排序
+bool cmp(const string a, const string b)
+{
+	if (strcmp(a.c_str(), b.c_str()) < 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 class Solution {
 public:
-	//vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+	vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+		vector<string> result;
+		for (int i = 0; i < words.size(); i++)
+		{
+			if (findWord(board, words[i]) && find(result.begin(), result.end(), words[i])==result.end())
+			{
+				result.push_back(words[i]);
+			}
+		}
+		sort(result.begin(), result.end(),cmp);
+		return result;
+	}
 
-	//}
 
 	bool findWord(vector<vector<char>> board, string target)
 	{
 		stack<Pos> stackPos;
+		vector<vector<bool>> visited;
+
+		if (board.empty())
+		{
+			return false;
+		}
+		int i = 0;
+		int j = 0;
+		vector<bool> tempVec;
+		while (i<board.size())
+		{
+			tempVec.clear();
+			j = 0;
+			while (j < board[0].size())
+			{
+				tempVec.push_back(false);
+				j++;
+			}
+			visited.push_back(tempVec);
+			i++;
+		}
+
 		int numR = board.size();
 		int numC = board[0].size();
 		Pos start = {0,0};
@@ -34,74 +80,88 @@ public:
 		// 找到首字母
 		// 找到结果，return
 		// 找到最后位置，return
-		int i = 0;
-		int j = 0;
+		i = 0;
+		j = 0;
 		int x = 0;
 		int y = 0;
+		int count = 0;
+		int count2 = 0;
 		bool found = false;
 		while (true)
 		{
+			count++;
 			if (board[i][j]==target[0])
 			{
-				index++;
-				start = { x,y };
+				if (target.length() == 1)
+				{
+					return true;
+				}
+
+				start = { i,j };
 				stackPos.push(start);
+				visited[i][j] = true;
+
+				index = 1;	// 查找到了第一个字母之后，index变为1
 				while (!stackPos.empty())
 				{
+					count2++;
 					temp = stackPos.top();
 					x = temp.x;
 					y = temp.y;
 
 					// 从4个方向查找target[index]
-					//direction = 0;
 					found = false;
 					while (direction < 4)
 					{
 						switch (direction)
 						{
 						case 0:
-							if (x != 0)
+							if (x != 0 && !visited[x-1][y])
 							{
 								if (board[x-1][y] == target[index])
 								{
 									temp = {--x,y};
 									stackPos.push(temp);
+									visited[x][y] = true;
 									index++;
 									found = true;
 								}
 							}
 							break;
 						case 1:
-							if (x != numC - 1)
+							if (y != numC - 1 && !visited[x][y+1])
 							{
 								if (board[x][y+1] == target[index])
 								{
 									temp = { x,++y };
 									stackPos.push(temp);
+									visited[x][y] = true;
 									index++;
 									found = true;
 								}
 							}
 							break;
 						case 2:
-							if (x != numR - 1)
+							if (x != numR - 1 && !visited[x + 1][y])
 							{
 								if (board[x + 1][y] == target[index])
 								{
 									temp = { ++x,y };
 									stackPos.push(temp);
+									visited[x][y] = true;
 									index++;
 									found = true;
 								}
 							}
 							break;
 						case 3:
-							if (y != 0)
+							if (y != 0 && !visited[x][y-1])
 							{
 								if (board[x][y - 1] == target[index])
 								{
 									temp = { x,--y };
 									stackPos.push(temp);
+									visited[x][y] = true;
 									index++;
 									found = true;
 								}
@@ -111,7 +171,7 @@ public:
 						}
 						if (found)
 						{
-							if (index == target.length() - 1)
+							if (index == target.length())
 							{
 								return true;
 							}
@@ -127,7 +187,25 @@ public:
 					{
 						temp = stackPos.top();
 						stackPos.pop();
+						visited[temp.x][temp.y] = false;
 						index--;
+
+						// 如果栈中每个节点都是上一个节点的最后一个方向，则直接结束查找
+						while (!stackPos.empty() && temp.y == stackPos.top().y - 1)
+						{
+							temp = stackPos.top();
+							stackPos.pop();
+							visited[temp.x][temp.y] = false;
+							index--;
+						}
+
+						// 如果已经将栈里的路径排除了，需要重新选择第一个字符
+						if (stackPos.empty())
+						{
+							direction = 0;
+							break;
+						}
+
 						if (temp.x == stackPos.top().x - 1)
 						{
 							direction = 1;
@@ -140,11 +218,10 @@ public:
 						{
 							direction = 3;
 						}
-						else if (temp.y == stackPos.top().y - 1)
-						{
-							direction = 0;
-							return false;
-						}
+					}
+					else
+					{
+						direction = 0;
 					}
 				}
 			}
@@ -168,3 +245,70 @@ public:
 		}
 	}
 };
+
+
+class TrieNode {
+public:
+	bool is_end;
+	vector<TrieNode*> children;
+	TrieNode() {
+		is_end = false;
+		children = vector<TrieNode*>(26, NULL);
+	}
+};
+
+// 别人的做法
+//class Trie {
+//public:
+//	TrieNode* getRoot() { return root; }
+//	Trie(vector<string>& words) {
+//		root = new TrieNode();
+//		for (int i = 0; i<words.size(); ++i)
+//			addWord(words[i]);
+//	}
+//	void addWord(const string& word) {
+//		TrieNode* cur = root;
+//		for (int i = 0; i<word.size(); ++i) {
+//			int index = word[i] - 'a';
+//			if (cur->children[index] == NULL)
+//				cur->children[index] = new TrieNode();
+//			cur = cur->children[index];
+//		}
+//		cur->is_end = true;
+//	}
+//private:
+//	TrieNode* root;
+//};
+//
+//class Solution {
+//public:
+//	vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+//		Trie* trie = new Trie(words);
+//		TrieNode* root = trie->getRoot();
+//		set<string> result_set;
+//		for (int x = 0; x<board.size(); ++x)
+//			for (int y = 0; y<board[0].size(); ++y)
+//				findWords(board, x, y, root, "", result_set);
+//
+//		vector<string> result;
+//		for (auto it : result_set)    result.push_back(it);
+//		return result;
+//	}
+//private:
+//	void findWords(vector<vector<char>>& board, int x, int y, TrieNode* root, string word, set<string>& result) {
+//		if (x<0 || x >= board.size() || y<0 || y >= board[0].size() || board[x][y] == ' ') return;
+//
+//		if (root->children[board[x][y] - 'a'] != NULL) {
+//			word = word + board[x][y];
+//			root = root->children[board[x][y] - 'a'];
+//			if (root->is_end) result.insert(word);
+//			char c = board[x][y];
+//			board[x][y] = ' ';
+//			findWords(board, x + 1, y, root, word, result);
+//			findWords(board, x - 1, y, root, word, result);
+//			findWords(board, x, y + 1, root, word, result);
+//			findWords(board, x, y - 1, root, word, result);
+//			board[x][y] = c;
+//		}
+//	}
+//};
