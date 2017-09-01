c32877a701530783da675c65eecfba0e4381994f
sunRainPenguin <1049590822@qq.com>
2017-09-01 15:38:04 +0800

    update2

--- wordSearch/wordSearch/Solution.cpp
+++ wordSearch/wordSearch/Solution.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-	vector<vector<char>> board = { {'a','a','a','a'},{ 'a','a','a','a' },{ 'a','a','a','a' },{ 'a','a','a','a' },{ 'b','c','d','e' },{ 'f','g','h','i' },{ 'j','k','l','m' },{ 'n','o','p','q' },{ 'r','s','t','u' },{ 'v','w','x','y' },{ 'z','z','z','z' }};// { { 'b','a','a','b','a','b' }, { 'a','b','a','a','a','a' }, { 'a','b','a','a','a','b' }, { 'a','b','a','b','b','a' },{ 'a','a','b','b','a','b' },{ 'a','a','b','b','b','a' },{ 'a','a','b','a','a','b' } };	// { {'o'},{'a'},{'t'},{'x'} }; { 'o','a','a','n' },{ 'e','t','a','e' },{ 'i','h','k','r' },{ 'i','f','l','v' }
+	vector<vector<char>> board = { { 'o','a','a','n' },{ 'e','t','a','e' },{ 'i','h','k','r' },{ 'i','f','l','v' } };// { { 'b','a','a','b','a','b' }, { 'a','b','a','a','a','a' }, { 'a','b','a','a','a','b' }, { 'a','b','a','b','b','a' },{ 'a','a','b','b','a','b' },{ 'a','a','b','b','b','a' },{ 'a','a','b','a','a','b' } };	// { {'o'},{'a'},{'t'},{'x'} }; 
 	Solution solution;
-	vector<string> inputs = { "aaaaaaaaaaaaaaai","aaaaaaaaaaaaaaaj","aaaaaaaaaaaaaaak","aaaaaaaaaaaaaaal","aaaaaaaaaaaaaaam","aaaaaaaaaaaaaaan","aaaaaaaaaaaaaaao","aaaaaaaaaaaaaaap" };
+	vector<string> inputs = { "oath","pea","eat","rain"};
 	vector<string> vecStr = solution.findWords(board, inputs);
 	for (int i = 0; i < vecStr.size(); i++)
 	{
